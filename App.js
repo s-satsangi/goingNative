571dc544e40d05acc2090dab3750dc28ef11f897
s-satsangi <s.satsangi@gmail.com>
2021-04-18 23:44:40 -0400

    got images working woo

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 
 export default function App() {
   const handlePress = () => console.log("Ouch!");
   return (
     <SafeAreaView style={styles.container}>
       <Text numberOfLines={1} onPress={handlePress}>
-        Yooooooooooooooooooooooo! The bucket sort algorithm is a distributed
-        sorting algorithm that separates the elements into different buckets, or
-        smaller arrays, and then uses a simpler sorting algorithm good for
-        sorting small arrays, such as insertion sort, to sort each bucket
+        Yooooooooooooooooooooooo!
       </Text>
+      <Image
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/id/237/200/300",
+        }}
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -20,9 +24,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "dodgerblue",
-    // alignItems: "center",
-    // justifyContent: "center",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
     paddingTop: 25,
   },
 });
